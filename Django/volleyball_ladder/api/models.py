095ce8c89962061e29f5ae2f98ead0bd763b5c00
Jonah Burke <burke099@csusm.edu>
2024-02-21 11:45:06 -0800

    added a table user

--- Django/volleyball_ladder/api/models.py
+++ Django/volleyball_ladder/api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class user(models.Model):
+    username = models.CharField(max_length=55)
+    email = models.CharField(max_length=55)
+    password = models.CharField(max_length=55)
+    profilePic = models.ImageField()
