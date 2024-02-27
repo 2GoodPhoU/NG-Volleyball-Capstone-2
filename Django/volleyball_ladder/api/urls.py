9836b6f569b6953661549a026d2033810551a78b
Wobie123 <kennethha47@gmail.com>
2024-02-27 08:41:13 -0800

    added some test cases for the db
    Current work in progess but going to push to prod

--- Django/volleyball_ladder/api/urls.py
+++ Django/volleyball_ladder/api/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import UserView
+from .views import UserView, TeamView, DivisonsView, MatchTableView, CourtScheduleView
 
 urlpatterns = [
-    path('user', UserView.as_view())
+    path('User', UserView.as_view()),
+    path('Team', TeamView.as_view()),
+    path('Divisons', DivisonsView.as_view()),
+    path('MatchTable', MatchTableView.as_view()),
+    path('CourtSchedule', CourtScheduleView.as_view())
 ]
