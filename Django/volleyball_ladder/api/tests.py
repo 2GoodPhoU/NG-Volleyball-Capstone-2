5e3dc95ca46b33050c83ae0c431d4f33b5cfa995
Wobie123 <kennethha47@gmail.com>
2024-02-27 08:41:13 -0800

    added some test cases for the db
    Current work in progess but going to push to prod

--- Django/volleyball_ladder/api/tests.py
+++ Django/volleyball_ladder/api/tests.py
@@ -1,6 +1,29 @@
 from django.test import TestCase
+from .models import User, Team, Divisons, MatchTable, CourtSchedule
 
-# Create your tests here.
+#python manage.py test -to run test, it resets every time
+#python manage.py flush
+class ModelTestCase(TestCase):
+    """def setUp(self):
+        # Create test data
+        self.user = User.objects.create(username='testuser', email='test@example.com', password='password123')
+        self.team = Team.objects.create(teamName='Test Team', captain=self.user)
+        self.division = Divisons.objects.create(teamName=self.team, position=1, admin=self.user)
+        self.match = MatchTable.objects.create(team1Name=self.team, team2Name=self.team)
+        self.court_schedule = CourtSchedule.objects.create(location='Court 1', id=self.match)
 
+    def test_user_creation(self):
+        # Test user creation
+        user = User.objects.get(username='testuser')
+        self.assertEqual(user.email, 'test@example.com')
 
+    def test_team_creation(self):
+        # Test team creation
+        team = Team.objects.get(teamName='Test Team')
+        self.assertEqual(team.captain, self.user)
+    """
+    def CreateUser(self):
+        self.user = User.objects.create(username='testuser', email='test@example.com', password='password123')
 
+    def CreateTeam(slef):
+        self.team = Team.objects.create(teamName='Test Team', captain=self.user)
