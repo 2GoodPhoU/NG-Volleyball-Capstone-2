d6adc76485f7458023d9562f8991c4b0c2c99df5
Jonah Burke <burke099@csusm.edu>
2024-03-05 08:06:29 -0800

    edited api/views to have function

--- Django/volleyball_ladder/api/views.py
+++ Django/volleyball_ladder/api/views.py
@@ -8,3 +8,19 @@ from .serializers import MatchTableSerializer,CourtScheduleSerializer
 class UserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class TeamView(generics.CreateAPIView):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
+
+class DivisonsView(generics.CreateAPIView):
+    queryset = Divisons.objects.all()
+    serializer_class = DivisonsSerializer
+
+class MatchTableView(generics.CreateAPIView):
+    queryset = MatchTable.objects.all()
+    serializer_class = MatchTableSerializer
+
+class CourtScheduleView(generics.CreateAPIView):
+    queryset = CourtSchedule.objects.all()
+    serializer_class = CourtScheduleSerializer
